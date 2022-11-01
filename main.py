614ea9213ddd6cb2941180575be5139107d74dba
Bobur <boburjon@usf.edu>
2022-11-01 18:41:54 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from logic import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("SnakeGame")
+screen.tracer(0)
+segments = []
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 260 or snake.head.ycor() < -300:
+        scoreboard.reset()
+        snake.reset()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+
+
+
+
+screen.exitonclick()
